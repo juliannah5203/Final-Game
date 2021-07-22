9d26f60b4e1f7a8665b64466f14923037d60a273
Qiuting Zhao <qzhao29@ucsc.edu>
2021-07-22 08:41:19 -0700

    finish all

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 // Group Name: Seg Fault
 // Group Member: Julianne He, Lexin Xiong, Qiuting Zhao
-
 let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 640,
-    scene: [ Menu, Level1, Level2, Level3, Level4, Level5, Level6, End ] 
+    scene: [ Menu, Play1, Level1, Level2, Level3, End ] 
 }
 let game = new Phaser.Game(config);
 // reserve keyboard vars
